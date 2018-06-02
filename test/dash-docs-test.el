a509ed369d092efd123290f221671c46335f04a5
Bryan Gilbert <gilbertw1@gmail.com>
2018-06-02 12:28:21 -0400

    Update tests and remove example gif

--- test/dash-docs-test.el
+++ test/dash-docs-test.el
@@ -1,4 +1,4 @@
-;;; helm-dash-test.el --- Helm-dash tests
+;;; dash-docs-test.el --- dash-docs tests
 ;; Copyright (C) 2013-2014  Raimon Grau
 ;; Copyright (C) 2013-2014  Toni Reina
 
@@ -25,146 +25,146 @@
 
 ;;; Code:
 
-;;;; helm-dash-maybe-narrow-docsets
+;;;; dash-docs-maybe-narrow-docsets
 
-(ert-deftest helm-dash-maybe-narrow-docsets-test/filtered ()
+(ert-deftest dash-docs-maybe-narrow-docsets-test/filtered ()
   "Should return a list with filtered connections."
   (let ((pattern "Go ")
-        (helm-dash-docsets-path "/tmp/.docsets")
-        (helm-dash-common-docsets '("Redis" "Go" "CSS" "C" "C++"))
-        (helm-dash-connections
+        (dash-docs-docsets-path "/tmp/.docsets")
+        (dash-docs-common-docsets '("Redis" "Go" "CSS" "C" "C++"))
+        (dash-docs-connections
          '(("Redis" "/tmp/.docsets/Redis.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("C" "/tmp/.docsets/C.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("C++" "/tmp/.docsets/C++.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("CSS" "/tmp/.docsets/CSS.docset/Contents/Resources/docSet.dsidx" "ZDASH"))))
-    (should (equal (helm-dash-maybe-narrow-docsets pattern)
+    (should (equal (dash-docs-maybe-narrow-docsets pattern)
                    '(("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH"))))
 
-    (should (equal "C" (caar (helm-dash-maybe-narrow-docsets "C foo"))))
-    (should (equal "C++" (caar (helm-dash-maybe-narrow-docsets "C++ foo"))))
-    (should (equal "C" (caar (helm-dash-maybe-narrow-docsets "c foo"))))))
+    (should (equal "C" (caar (dash-docs-maybe-narrow-docsets "C foo"))))
+    (should (equal "C++" (caar (dash-docs-maybe-narrow-docsets "C++ foo"))))
+    (should (equal "C" (caar (dash-docs-maybe-narrow-docsets "c foo"))))))
 
-(ert-deftest helm-dash-maybe-narrow-docsets-test/not-filtered ()
+(ert-deftest dash-docs-maybe-narrow-docsets-test/not-filtered ()
   "Should return all current connections because the pattern doesn't match with any connection."
   (let ((pattern "FOOOO ")
-	(helm-dash-docsets-path "/tmp/.docsets")
-	(helm-dash-common-docsets '("Redis" "Go" "CSS"))
-	(helm-dash-connections
+	(dash-docs-docsets-path "/tmp/.docsets")
+	(dash-docs-common-docsets '("Redis" "Go" "CSS"))
+	(dash-docs-connections
 	 '(("Redis" "/tmp/.docsets/Redis.docset/Contents/Resources/docSet.dsidx" "DASH")
 	   ("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH")
 	   ("CSS" "/tmp/.docsets/CSS.docset/Contents/Resources/docSet.dsidx" "ZDASH"))))
-    (should (equal (helm-dash-maybe-narrow-docsets pattern) helm-dash-connections))))
+    (should (equal (dash-docs-maybe-narrow-docsets pattern) dash-docs-connections))))
 
 
-;;;; helm-dash-sub-docset-name-in-pattern
+;;;; dash-docs-sub-docset-name-in-pattern
 
-(ert-deftest helm-dash-sub-docset-name-in-pattern-test/with-docset-name ()
+(ert-deftest dash-docs-sub-docset-name-in-pattern-test/with-docset-name ()
   ""
   (let ((pattern "Redis BLPOP")
 	(docset "Redis"))
-    (should (equal (helm-dash-sub-docset-name-in-pattern pattern docset) "BLPOP"))))
+    (should (equal (dash-docs-sub-docset-name-in-pattern pattern docset) "BLPOP"))))
 
-(ert-deftest helm-dash-sub-docset-name-in-pattern-test/without-docset-name ()
+(ert-deftest dash-docs-sub-docset-name-in-pattern-test/without-docset-name ()
   ""
   (let ((pattern "BLPOP")
 	(docset "Redis"))
-    (should (equal (helm-dash-sub-docset-name-in-pattern pattern docset) pattern))))
+    (should (equal (dash-docs-sub-docset-name-in-pattern pattern docset) pattern))))
 
-(ert-deftest helm-dash-sub-docset-name-in-pattern-test/with-special-docset-name ()
+(ert-deftest dash-docs-sub-docset-name-in-pattern-test/with-special-docset-name ()
   ""
   (let ((pattern "C++ printf")
 	(docset "C++"))
-    (should (equal (helm-dash-sub-docset-name-in-pattern pattern docset) "printf"))))
+    (should (equal (dash-docs-sub-docset-name-in-pattern pattern docset) "printf"))))
 
-;;;; helm-dash-result-url
+;;;; dash-docs-result-url
 
-(ert-deftest helm-dash-result-url/checks-docset-types ()
+(ert-deftest dash-docs-result-url/checks-docset-types ()
   (should (string-match-p "Documents/three#anchor$"
-                          (helm-dash-result-url "Python 3" "three" "anchor")))
+                          (dash-docs-result-url "Python 3" "three" "anchor")))
   (should (string-match-p "Documents/three#anchor$"
-                          (helm-dash-result-url "Css" "three#anchor" nil)))
+                          (dash-docs-result-url "Css" "three#anchor" nil)))
   (should (string-match-p "Documents/three#anchor$"
-                          (helm-dash-result-url "Redis" "three#anchor"))))
+                          (dash-docs-result-url "Redis" "three#anchor"))))
 
-(ert-deftest helm-dash-result-url/remove-dash-entry-tag-from-url ()
+(ert-deftest dash-docs-result-url/remove-dash-entry-tag-from-url ()
   (should (string-match-p "Documents/three#anchor$"
-                          (helm-dash-result-url "Python 3" "three<dash_entry_menuDescription=android.provider.ContactsContract.CommonDataKinds.Website>" "anchor"))))
+                          (dash-docs-result-url "Python 3" "three<dash_entry_menuDescription=android.provider.ContactsContract.CommonDataKinds.Website>" "anchor"))))
 
-;;;; helm-dash-docsets-path
+;;;; dash-docs-docsets-path
 
-(ert-deftest helm-dash-docsets-path-test/relative-path ()
+(ert-deftest dash-docs-docsets-path-test/relative-path ()
   "Should return the absolute path."
-  (let ((helm-dash-docsets-path "~/.emacs.d/helm-dash-docsets")
+  (let ((dash-docs-docsets-path "~/.emacs.d/dash-docs-docsets")
 	(home-path (getenv "HOME")))
-    (should (equal (helm-dash-docsets-path)
-		   (format "%s/.emacs.d/helm-dash-docsets" home-path)))))
+    (should (equal (dash-docs-docsets-path)
+		   (format "%s/.emacs.d/dash-docs-docsets" home-path)))))
 
-;;;; helm-dash-add-to-kill-ring
+;;;; dash-docs-add-to-kill-ring
 
-(ert-deftest helm-dash-add-to-kill-ring-test ()
-  "Should add to kill ring a string with a call to `helm-dash-browse-url'"
+(ert-deftest dash-docs-add-to-kill-ring-test ()
+  "Should add to kill ring a string with a call to `dash-docs-browse-url'"
   (let ((results '(Redis ("func" "Documents/blpop.html"))))
-    (helm-dash-add-to-kill-ring results))
+    (dash-docs-add-to-kill-ring results))
   (should (equal (current-kill 0 t)
-		 "(helm-dash-browse-url '(Redis (\"func\" \"Documents/blpop.html\")))")))
+		 "(dash-docs-browse-url '(Redis (\"func\" \"Documents/blpop.html\")))")))
 
-;;;; helm-dash-official-docsets
+;;;; dash-docs-official-docsets
 
-(ert-deftest helm-dash-official-docsets-test ()
+(ert-deftest dash-docs-official-docsets-test ()
   "Should return a list of available docsets."
-  (let ((docsets (helm-dash-official-docsets)))
+  (let ((docsets (dash-docs-official-docsets)))
     (should (member "Ruby" docsets))
     ;; ignored docset:
     (should-not (member "Man_Pages" docsets))))
 
-;;;; helm-dash-activate-docset
+;;;; dash-docs-activate-docset
 
-(ert-deftest helm-dash-activate-docset ()
-  (let ((helm-dash-common-docsets '("Redis" "Go" "CSS"))
-	(helm-dash-connections
+(ert-deftest dash-docs-activate-docset ()
+  (let ((dash-docs-common-docsets '("Redis" "Go" "CSS"))
+	(dash-docs-connections
 	 '(("Redis" "/tmp/.docsets/Redis.docset/Contents/Resources/docSet.dsidx" "DASH")
 	   ("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH")
 	   ("CSS" "/tmp/.docsets/CSS.docset/Contents/Resources/docSet.dsidx" "ZDASH"))))
-    (helm-dash-activate-docset "Clojure")
-    (should (equal'("Clojure" "Redis" "Go" "CSS") helm-dash-common-docsets))
-    (should (equal nil helm-dash-connections))))
+    (dash-docs-activate-docset "Clojure")
+    (should (equal'("Clojure" "Redis" "Go" "CSS") dash-docs-common-docsets))
+    (should (equal nil dash-docs-connections))))
 
-;; helm-dash-buffer-local-docsets
+;; dash-docs-buffer-local-docsets
 
-(ert-deftest helm-dash-buffer-local-docsets-narrowing ()
+(ert-deftest dash-docs-buffer-local-docsets-narrowing ()
   (let ((c-buffer nil)
-        (helm-dash-connections
+        (dash-docs-connections
          '(("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("C" "/tmp/.docsets/C.docset/Contents/Resources/docSet.dsidx" "DASH")
            ("CSS" "/tmp/.docsets/CSS.docset/Contents/Resources/docSet.dsidx" "ZDASH"))))
     (with-temp-buffer
       (setq c-buffer (current-buffer))
-      (setq-local helm-dash-docsets (list "C"))
+      (setq-local dash-docs-docsets (list "C"))
 
       (with-temp-buffer
-        (setq-local helm-dash-docsets (list "Go"))
-        (should (equal (helm-dash-maybe-narrow-docsets "*")
+        (setq-local dash-docs-docsets (list "Go"))
+        (should (equal (dash-docs-maybe-narrow-docsets "*")
                        '(("Go" "/tmp/.docsets/Go.docset/Contents/Resources/docSet.dsidx" "DASH"))))
 
         (with-current-buffer c-buffer
-          (should (equal (helm-dash-maybe-narrow-docsets "*")
+          (should (equal (dash-docs-maybe-narrow-docsets "*")
                          '(("C" "/tmp/.docsets/C.docset/Contents/Resources/docSet.dsidx" "DASH"))))
           )))))
 
-;; helm-dash-sql-query
+;; dash-docs-sql-query
 
-(ert-deftest helm-dash-sql-query/DASH-docset-type ()
+(ert-deftest dash-docs-sql-query/DASH-docset-type ()
   (should (equal "SELECT t.type, t.name, t.path FROM searchIndex t WHERE t.name like '%blpop%' ORDER BY LENGTH(t.name), LOWER(t.name) LIMIT 1000"
-		 (helm-dash-sql-query "DASH" "blpop"))))
+		 (dash-docs-sql-query "DASH" "blpop"))))
 
-(ert-deftest helm-dash-sql-query/ZDASH-docset-type ()
+(ert-deftest dash-docs-sql-query/ZDASH-docset-type ()
   (should (equal "SELECT ty.ZTYPENAME, t.ZTOKENNAME, f.ZPATH, m.ZANCHOR FROM ZTOKEN t, ZTOKENTYPE ty, ZFILEPATH f, ZTOKENMETAINFORMATION m WHERE ty.Z_PK = t.ZTOKENTYPE AND f.Z_PK = m.ZFILE AND m.ZTOKEN = t.Z_PK AND t.ZTOKENNAME like '%blpop%' ORDER BY LENGTH(t.ZTOKENNAME), LOWER(t.ZTOKENNAME) LIMIT 1000"
-		 (helm-dash-sql-query "ZDASH" "blpop"))))
+		 (dash-docs-sql-query "ZDASH" "blpop"))))
 
-(ert-deftest helm-dash-sql-query/UNKNOWN-docset-type ()
-  (should (equal nil (helm-dash-sql-query "FOO" "blpop"))))
+(ert-deftest dash-docs-sql-query/UNKNOWN-docset-type ()
+  (should (equal nil (dash-docs-sql-query "FOO" "blpop"))))
 
-(provide 'helm-dash-test)
+(provide 'dash-docs-test)
 
-;;; helm-dash-test ends here
+;;; dash-docs-test ends here
